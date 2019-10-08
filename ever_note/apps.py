2753d04fabc9fd14dab055fec216ec859484f0c0
junaidtariq <junaidtariq166@gmail.com>
2019-10-08 13:09:34 +0500

    updated version

---
+++ ever_note/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EverNoteConfig(AppConfig):
+    name = 'ever_note'
--- ever_note/urls.py
+++ ever_note/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import include, url
 from ever_note import views
 
 urlpatterns = [
-    path('evernote_home/', views.EvernoteHome.as_view(), name='evernote_home'),
-    path('evernote_connect/', views.EvernoteHome.as_view(), name='evernote_connect'),
+
 ]
